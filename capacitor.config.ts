2a93e7cb541ac001a28485eb953e49d1161449c6
raiza3983-oss <raiza3983@gmail.com>
2026-05-23 16:51:01 -0300

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.feiralivre.app',
+  appName: 'Feira Livre',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
